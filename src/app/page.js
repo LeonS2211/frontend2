66d8cd883a6e6daea789935918a26bbbf9327eb5
LeonS2211 <lgse1021@gmail.com>
2023-07-21 19:09:42 -0500

    fixes para iniciar frontend

--- src/app/page.js
+++ src/app/page.js
@@ -1,86 +1,29 @@
 'use client'
 
+import UniversidadesApi from './api/universidades.js';
 import styles from './page.module.css'
 import Link from '../components/Link/Link.jsx';
-import Button from '@/components/Button/Button';
-import Form from 'react-bootstrap/Form';
-
-
-  const basedatos = [{
-      email: "Leo",
-      password: "broder1843",
-      nombre: "Leonardo",
-      apellido:"Salazar",
-      tipoDoc:"DNI",
-      nDoc:"76569005",
-      role:"alumno",
-    },
-    {
-        email: "Alfredo",
-        password: "michi123",
-        nombre: "Alfred",
-        apellido:"Pompeyo",
-        tipoDoc:"DNI",
-        nDoc:"12345678",
-        role:"profesor",
-        curso:"Programacion Web",
-        horarios:"20/06/2023"
-    },
-    {
-        email: "Alfredo",
-        password: "michi123",
-        nombre: "Alex",
-        apellido:"Pompeyo",
-        tipoDoc:"DNI",
-        nDoc:"12345678",
-        role:"profesor",
-        curso:"Programacion Web4",
-        horarios:"21/06/2023"
-    }
-    ,
-    {
-        email: "Alfredo",
-        password: "michi123",
-        nombre: "Alfrederic",
-        apellido:"Pompeyo",
-        tipoDoc:"DNI",
-        nDoc:"12345678",
-        role:"profesor",
-        curso:"Programacion Web3",
-        horarios:"24/06/2023"
-    },
-    {
-        email: "Mitchel",
-        password: "michi123",
-        nombre: "Mitchel",
-        apellido:"Pompeyo",
-        tipoDoc:"DNI",
-        nDoc:"12345678",
-        role:"profesor",
-        curso:"Programacion Web2",
-        horarios:"18/06/2023"
-    }
-  ];
-
-localStorage.setItem('loggedIn', null)
-
-const handleReset = (event) =>{
-    event.preventDefault()
-    localStorage.setItem('basedatos',JSON.stringify(basedatos))
-}
+import { useEffect, useState } from "react"
 
 export default function Home() {
+  const [ universidad, setUniversidad ] = useState()
+
+const handleOnLoad = async () => {
+  const result = await UniversidadesApi.findAll();
+  setUniversidad(result.data);
+  console.log(universidad)
+}
+
+useEffect(() => {
+  handleOnLoad();
+}, [])
+
   return (
     <div className={styles.inte}>
       <div className={styles.red}>
         <Link href="/login " text="BIENVENIDO AL MUNDO MAGICO DE GUMBALL" />
       </div>
-    <Form onSubmit={handleReset}>
-      <div className="op3">
-        <Button variant="danger" type="submit"  texto="Cargar/reset base de datos"/>
-      </div>
-    </Form>
-
+      <span>{universidad[1].descripcion}</span>
     </div>
 
   )
